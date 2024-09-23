682ee422590f51549c7751d218b097d66155e61f
jeonhyeonseon <162237209+jeonhyeonseon@users.noreply.github.com>
2024-09-23 13:12:20 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,15 +1,6 @@
 class Solution {
     public int solution(int n) {
-        // n = 피자를 나눠먹을 사람의 수
-        // answer = 피자의 조각 수
-        int answer = 0;
-        
-        if(n % 7 == 0){
-            answer = n / 7;
-        } else {
-            // 7로 나누었을 때 나머지가 있을 경우 피자를 추가한다.
-            answer = n / 7 + 1;
-        }
+        int answer = (n%7==0) ? n / 7 : n / 7 + 1;
         return answer;
     }
 }
