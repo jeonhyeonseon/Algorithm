dfa3e8004cc82123a55182a91036f127d79562b5
jeonhyeonseon <162237209+jeonhyeonseon@users.noreply.github.com>
2024-09-27 14:48:14 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.05 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -1,14 +1,8 @@
 class Solution {
     public String solution(String my_string, String letter) {
-        // char.At(0)을 사용해서 letter를 뺴기
-        // 그렇다면 for문을 이용해서 letter를 빼야하는게 맞는것인가. 
-        // -> for문을 사용하려는 이유 : my_string에 있는 문자열을 알기 위해서...?
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < my_string.length(); i++){
-            if(my_string.charAt(i) != letter.charAt(0)) {
-                sb.append(my_string.charAt(i));
-            }
-        }
-        return sb.toString();
+        String answer = "";
+        
+        answer = my_string.replace(letter, "");
+        return answer;
     }
 }
