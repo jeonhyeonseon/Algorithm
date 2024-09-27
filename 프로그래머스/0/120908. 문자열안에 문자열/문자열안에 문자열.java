917bddf690734580fec6f7b403706866c418dd0c
jeonhyeonseon <162237209+jeonhyeonseon@users.noreply.github.com>
2024-09-27 15:33:49 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.04 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,14 +1,5 @@
 class Solution {
     public int solution(String str1, String str2) {
-        int answer = 0;
-        
-        // str1 안에 str2가 포함되어 있는지 없는지 확인
-        if(str1.contains(str2)) {
-            answer = 1;
-        } else {
-            answer = 2;
-        }
-        
-        return answer;
+         return (str1.contains(str2) ? 1 : 2);
     }
 }
