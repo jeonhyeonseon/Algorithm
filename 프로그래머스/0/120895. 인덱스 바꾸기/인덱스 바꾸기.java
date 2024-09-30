8060a2f32baed19670c91673a88d9fbdf49f264f
jeonhyeonseon <162237209+jeonhyeonseon@users.noreply.github.com>
2024-09-30 16:29:45 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 11.17 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -1,12 +1,15 @@
 class Solution {
     public String solution(String my_string, int num1, int num2) {
-        // String을 char 배열로 변환하기
-        char[] charArr = my_string.toCharArray();
-        // 인덱스 num1자리에 num2 값 대입
-        charArr[num1] = my_string.charAt(num2);
-        // 인덱스 num2자리에 num1 값 대입
-        charArr[num2] = my_string.charAt(num1);
-        // char배열을 String 문자열로 변환
-        return String.valueOf(charArr);
+        String answer = "";
+        for(int i = 0; i < my_string.length(); i++){
+            if(i == num1) {
+                answer += my_string.charAt(num2);
+            } else if (i == num2) {
+                answer += my_string.charAt(num1);
+            } else {
+                answer += my_string.charAt(i);
+            }
+        }
+        return answer;
     }
 }
