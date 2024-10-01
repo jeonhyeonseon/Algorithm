1f82d64aecbcaa7f073e3b7a86a7d45a1d17bd55
jeonhyeonseon <162237209+jeonhyeonseon@users.noreply.github.com>
2024-10-01 19:49:18 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.55 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/README.md
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 최댓값 만들기 (2) - 120862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120862#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120862) 
 
 ### 성능 요약
 
-메모리: 72.4 MB, 시간: 0.53 ms
+메모리: 78.3 MB, 시간: 0.55 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 01일 14:56:15
+2024년 10월 01일 19:49:16
 
 ### 문제 설명
 
--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -3,13 +3,13 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] numbers) {
         int answer = 0;
-        int mNum; // 음수 최대값
-        int pNum; // 양수 최소값
+        int negativeNum; // 음수 최대값
+        int positiveNum; // 양수 최소값
         Arrays.sort(numbers); // 오름차순으로 정렬
         
-        mNum = numbers[0] * numbers[1]; // 가장 작은 수끼리 곱하기
-        pNum = numbers[numbers.length - 1] * numbers[numbers.length - 2]; // 가장 큰 수끼리 곱하기
-        answer = Math.max(mNum, pNum);
+        negativeNum = numbers[0] * numbers[1]; // 가장 작은 수끼리 곱하기
+        positiveNum = numbers[numbers.length - 1] * numbers[numbers.length - 2]; // 가장 큰 수끼리 곱하기
+        answer = Math.max(negativeNum, positiveNum);
         return answer;
     }
 }
