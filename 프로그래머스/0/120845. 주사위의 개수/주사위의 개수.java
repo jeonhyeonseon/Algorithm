7fbcd58d521154ffc43ab3d88d22ab1f9a1ebe9c
jeonhyeonseon <162237209+jeonhyeonseon@users.noreply.github.com>
2024-10-01 19:40:29 +0900

    [level 0] Title: 주사위의 개수, Time: 0.03 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -1,5 +1,6 @@
 class Solution {
     public int solution(int[] box, int n) {
+        // n은 주사위의 모서리의 길이로 가로, 세로, 높이의 나머지를 구하여 이들을 곱한다.
         
         int a = box[0] / n; // 가로
         int b = box[1] / n; // 세로
