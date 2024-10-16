db901d0c3367e4d45ced851c969f7d10ab837007
jeonhyeonseon <162237209+jeonhyeonseon@users.noreply.github.com>
2024-10-16 21:03:47 +0900

    [level 0] Title: 가까운 수, Time: 1.12 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/README.md
+++ 프로그래머스/0/120890. 가까운 수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 가까운 수 - 120890 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120890#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120890) 
 
 ### 성능 요약
 
-메모리: 73.2 MB, 시간: 1.33 ms
+메모리: 67.2 MB, 시간: 1.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 12일 23:34:33
+2024년 10월 16일 21:03:46
 
 ### 문제 설명
 
--- 프로그래머스/0/120890. 가까운 수/가까운 수.java
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -3,21 +3,21 @@ import java.util.*;
 class Solution {
     public int solution(int[] array, int n) {
         
-        // 배열을 오름차순으로 정렬
+        // 먼저 배열을 오름차순으로 정렬한다
         Arrays.sort(array);
         
-        // 정답을 저장할 변수, 초기값으로 첫 번째 요소를 저장
+        // 정답을 저장할 변수, 초기값으로 첫 번째 요소를 저장한다.
         int result = array[0];
         
-        // 배열 요소와 n의 차이를 저장하는 변수 (절대값으로 저장)
-        int diff = Math.abs(array[0] - n);  // 0번째 요소의 차이로 초기화
+        // 배열 요소와 n의 차이를 저장하는 변수 (절대값으로 저장해야한다)
+        int diff = Math.abs(array[0] - n);  // 0번째 요소의 차이로 초기화하기
         
-        // 0번째 요소는 이미 변수로 저장했기 때문에 1부터 시작
+        // 0번째 요소는 이미 변수로 저장했기 때문에 1부터 시작한다
         for (int i = 1; i < array.length; i++) {
-            // 현재 요소와 n의 차이 계산
+            // 현재 요소와 n의 차이 계산하기
             int currentDiff = Math.abs(array[i] - n);
             
-            // 현재 요소가 n에 더 가까운 경우 diff와 result를 갱신
+            // 현재 요소가 n에 더 가까운 경우 diff와 result를 갱신하기
             if (currentDiff < diff) {
                 diff = currentDiff;
                 result = array[i];
