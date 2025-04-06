f8197283717f1cc5f0ee2515b74681f3199713d7
jeonhyeonseon <162237209+jeonhyeonseon@users.noreply.github.com>
2025-04-06 18:17:30 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.04 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -1,14 +1,12 @@
 class Solution {
     public int solution(int a, int d, boolean[] included) {
         int answer = 0;
-        // 등차수열 공식 : n번째 항 = a + (n − 1) * d
+        // 등차수열 합 : a + (n - 1) * d
         
-        for(int i = 0; i < included.length; i++){ // i는 included의 인덱스
-            // included[i]가 true일 때 항들만 더한 값 리턴
+        for(int i = 0; i < included.length; i++){
             if(included[i] == true) {
                 answer += a + (i * d);
-                continue;
-            }
+            } 
         }
         
         return answer;
