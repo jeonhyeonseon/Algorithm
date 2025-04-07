0412882cb9ad319f284d42cde9903ec5175a7786
jeonhyeonseon <162237209+jeonhyeonseon@users.noreply.github.com>
2025-04-07 21:13:37 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.03 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -1,18 +1,22 @@
 class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
-        int sum = 0;
-        int mul = 1;
         
+        // 원소들의 곱 < 원소들의 합의 제곱 => 1 반환
+        // 원소들의 곱 > 원소들의 합의 제곱 => 0 반환
+        
+        int sum = 0;
+        int mul = 1; 
+        
+        // 반복문을 통해서 원소들의 합과 곱의 결과값을 구하는 식
         for(int i = 0; i < num_list.length; i++){
             sum += num_list[i];
             mul *= num_list[i];
         }
         
-        if (sum * sum > mul) {
+        if(sum * sum > mul) {
             answer = 1;
         }
-        
         return answer;
     }
 }
