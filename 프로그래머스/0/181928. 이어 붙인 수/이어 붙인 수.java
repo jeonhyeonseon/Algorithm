bfa68600d35a794f7d1ff4ca66164f38fd136d26
jeonhyeonseon <162237209+jeonhyeonseon@users.noreply.github.com>
2025-04-09 21:32:15 +0900

    [level 0] Title: 이어 붙인 수, Time: 1.82 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -1,9 +1,12 @@
 class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
+        
+        // 정수를 이어붙이려면 문자열로 변환후 값을 구하고 다시 정수로 변환한다.
         String odd = "";
         String even = "";
         
+        // 반복문을 통해서 홀수인지 짝수인지 판별 후 이어붙이기
         for(int i = 0; i < num_list.length; i++) {
             if(num_list[i] % 2 == 0) {
                 even += Integer.toString(num_list[i]);
@@ -12,7 +15,8 @@ class Solution {
             }
         }
         
-        answer = Integer.parseInt(odd) + Integer.parseInt(even);
+        // 문자열로 바뀐 홀수와 짝수를 다시 정수형으로 변환하여 합하기
+        answer = Integer.parseInt(even) + Integer.parseInt(odd);
         return answer;
     }
 }
