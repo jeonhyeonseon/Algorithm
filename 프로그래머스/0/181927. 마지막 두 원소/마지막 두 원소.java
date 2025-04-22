79b3ecff592c6436395ea7b87cac8645d1afed77
jeonhyeonseon <162237209+jeonhyeonseon@users.noreply.github.com>
2025-04-22 22:11:41 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -2,17 +2,19 @@ class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[num_list.length + 1];
         
-        int lastNum = num_list[num_list.length - 1];
-        int secondNum = num_list[num_list.length - 2];
+        // 마지막 원소와 마지막에서 두번째 원소 값 지정
+        int lastNumber = num_list[num_list.length - 1];
+        int secondNumber = num_list[num_list.length - 2];
         
+        // 배열 복사
         for(int i = 0; i < num_list.length; i++) {
             answer[i] = num_list[i];
         }
         
-        if(lastNum > secondNum) {
-            answer[num_list.length] = lastNum - secondNum;
+        if(lastNumber > secondNumber) {
+            answer[num_list.length] = lastNumber - secondNumber;
         } else {
-            answer[num_list.length] = lastNum * 2;
+            answer[num_list.length] = lastNumber * 2;
         }
         
         return answer;
