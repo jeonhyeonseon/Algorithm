83555b4143fcc92f9dc23809ff31e7206ca92fb2
jeonhyeonseon <162237209+jeonhyeonseon@users.noreply.github.com>
2025-10-27 19:18:41 +0900

    [Bronze V] Title: A-B, Time: 168 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Main{
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
         int B = sc.nextInt();
