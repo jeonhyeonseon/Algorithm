c3ee9b50da42b85788d66100342ad86d99a7fa32
jeonhyeonseon <162237209+jeonhyeonseon@users.noreply.github.com>
2025-12-16 16:59:05 +0900

    [Bronze V] Title: 개수 세기, Time: 212 ms, Memory: 17832 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        
+        for(int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int V = sc.nextInt();
+        int count = 0;
+        
+        for(int i = 0; i < N; i++) {
+            if(arr[i] == V) {
+                count++;
+            }
+        }
+        
+        System.out.println(count);
+    }
+}
